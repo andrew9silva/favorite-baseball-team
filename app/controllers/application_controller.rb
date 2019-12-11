23b9423872097f7553d89205285509ca45cb533f
Andrew Silva <andrewsilva9@gmail.com>
2019-12-11 00:50:56 +0000

    changed name of create_user to craete_account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,14 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
- get "/" do
-    erb :welcome
+  
+  def current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
   end
+
+  def is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
+
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,15 @@
 class UserController < ApplicationController
   
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
+  end
+  
+  get "/" do
+    erb :welcome
+  end
+  
   get '/login' do
     erb :'user/login'
   end 
