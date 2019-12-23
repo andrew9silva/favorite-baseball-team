a1323d7934f0b6c645f60c49df1ceffcb7e1078e
Andrew Silva <andrewsilva9@gmail.com>
2019-12-23 03:17:46 +0000

    do not need Comment Controller as of now

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,23 +1,6 @@
 class CommentController < ApplicationController 
 
   
-  get '/comments' do 
-    if !is_logged_in?(session)
-      redirect to '/login'
-    end
-    @user = current_user(session)
-    @comments = Comment.all 
-    erb :'comment/comments'
-  end
-  
-  post '/comments' do
-    user = current_user(session)
-    team = @team = Team.find_by_id(params[:id])
-    if params["content"].empty?
-      redirect to '/teams/all_teams'
-    end
-    comment = Comment.create(:content => params["content"], :user_id => user.id, :team_id => team.id)
-    redirect to '/comments'
-  end 
+ 
   
 end
