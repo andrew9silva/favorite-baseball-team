262fad4392a488c95a91937757335e724934e745
Andrew Silva <andrewsilva9@gmail.com>
2020-01-04 19:34:09 +0000

    Showing comments for user

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,6 +1,9 @@
 class CommentController < ApplicationController 
 
-  
- 
+  get '/comments' do
+    @comments = Comment.all 
+    @user = current_user(session)
+    erb :'comment/comments'
+  end 
   
 end
